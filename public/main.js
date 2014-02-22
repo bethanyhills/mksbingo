425acc93bd0de8f77650eb982dcbff1b10c922f8
Bethany Nagel <beth0851@gmail.com>
2014-02-21 20:39:12 -0600

    Add HTML and views folder

---
+++ public/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+
+	var usedArray = new Array(76)
+
+});
--- server.rb
+++ server.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'shotgun'
+require_relative 'lib/game_creator.rb'
+
+set :bind, '0.0.0.0' #vagrant vix
 
 get '/' do
   "Hey, this is a web app"
+  erb :layout
 end
