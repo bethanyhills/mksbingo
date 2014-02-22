1054113233905be2364e96b66f154d0fe1a93390
Patrick Brennan <chief125@gmail.com>
2014-02-22 11:41:03 -0600

    Added connection to database. Now pulls board data from Firebase if it exists on the server.

--- server.rb
+++ server.rb
@@ -5,8 +5,9 @@ require_relative 'lib/game_creator.rb'
 set :bind, '0.0.0.0' #vagrant vix
 
 get '/' do
-  @dual_array =Game_Board.randomizer()
+  @dual_array = Game_Board.randomizer()
   @gbt = @dual_array[1]
   @rand_arr = @dual_array[0]
+  @selected_arr = Game_Board.selected_spaces()
   erb :game_board
 end
