0c50c460b8607acf9a79a1cfd1357eccc185b47c
Bethany Nagel <beth0851@gmail.com>
2014-02-22 21:20:48 -0600

    Pull Github user info and add personalization to individual user board.

--- lib/github.rb
+++ lib/github.rb
@@ -1,9 +1,17 @@
 require 'rubygems'
 require 'unirest'
-require 'pry'
+
+
 
 class UserInfo
-    def self.gravatar_url(username)
-    	response = Unirest.get('https://api.github.com/user', {:params => {:access_token => access_token}})
+    def self.gravatar_url(access_token)
+    	response = Unirest.get('https://api.github.com/user/#{access_token}', {:params => {:access_token => access_token}})
     	gravatar = response.body["gravatar_id"]
     	"https://secure.gravatar.com/avatar/" + gravatar
+    end
+
+    def self.user_name(access_token)
+    	response = Unirest.get('https://api.github.com/user/#{access_token}', {:params => {:access_token => access_token}})
+    	username = response.body['login']
+    end
+end
