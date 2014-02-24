fa77833b1ab31b247cf1f72281085b63e59c9471
Patrick Brennan <chief125@gmail.com>
2014-02-23 19:00:28 -0600

    Error in server that was trying to grab session fixed

--- server.rb
+++ server.rb
@@ -11,10 +11,12 @@ CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
 CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 
 get '/' do
-  if session[:auth_result]['name'] == ""
-    @name = session[:auth_result]['login']
-  else
-    @name = session[:auth_result]['name']
+  if session[:auth_result]
+    if session[:auth_result]['name'] == ""
+      @name = session[:auth_result]['login']
+    else
+      @name = session[:auth_result]['name']
+    end
   end
   @photo = session[:auth_result]['avatar_url'] if session[:auth_result]
   erb :index, :locals => {:client_id => CLIENT_ID}
